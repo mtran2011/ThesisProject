ff930643eaed8575b0dcc884998f431653d5f8e2
Minh Ha Tran <tranhaminh@gmail.com>
2017-09-19 23:54:49 -0400

    update

--- source/stock.py
+++ source/stock.py
@@ -1,2 +1,23 @@
 import abc
 import random
+
+class Stock(object):
+    ''' Abstract base class for a stock object
+    Attributes:
+        spot (float): the current spot price, must be rounded to 2 decimal points (cents)
+    '''
+    
+    __metaclass__ = abc.ABCMeta
+    
+    def __init__(self, spot):
+        if not spot:
+            raise ValueError('spot price input cannot be None')
+        self.spot = round(spot, 2)
+        
+    def simulate_price(self, mu, sigma, dt):
+        ''' Use dS = mu * dt + sigma * sqrt(dt) * standard normal N(0,1)
+        Based on dS = mu(.) * dt + sigma(.) * dW where var(dW) = dt        
+        '''
+        new_spot = self.spot + mu * dt + sigma * (dt**0.5) * random.gauss(0.0, 1.0)
+        self.spot = round(new_spot, 2)
+        return self.spot
