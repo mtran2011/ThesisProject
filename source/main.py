36a0bcf67fc1fc6f82e1afd1b5f8958694f52d54
Minh Ha Tran <tranhaminh@gmail.com>
2017-09-30 00:36:09 -0400

    save

--- source/main.py
+++ source/main.py
@@ -1,41 +1,36 @@
 from stock import OUStock
 
-def main():
-    '''
-    initialize new ou stock(price=10, *lambda)  
-    action_space = [-100,0,100,200]
-    
-    show agent state=(price=10, owned=0)
-    
-    agent loops over action_space:
-        based on state=(price=10, owned=0), find highest q, and thus best action 
-        return action = buy 200
-    
-    environment:
-        execute order to buy 200, bump price which means transaction cost of 0.01
-        simulate price from 10 to 11
-        calculate reward = 1 + transaction cost = 0.99
-        give agent reward, new state=(price=9, owned=200)
-        
-    agent update q using reward=0.99, newstate      
-    '''
+def run()
+
+def main():    
     stock = OUStock(10.52, kappa=0.1, mu=16.0, sigma=0.3/252)
-    lot, tick = 100, 0.01
-    actions = range(-10*lot, 11*lot, lot)
-    qlearner = QLearner(actions)    
-    exchange = StockExchange(stock, lot, tick, impact=0)
-    reward = 0
-    state = (stock.get_price(), shares_owned=0)
-    for _ in range(1000000):
-        order = qlearner.learn(reward, state)
-        exchange.execute(order) 
-        exchange.update_position_count()
-        exchange.report_transaction_cost()        
-        exchange.simulate_stock_price()
-        reward = exchange.calculate_pnl()
-        state = (exchange.report_stock_price(), exchange.report_position_count())
-        
+    lot, tick = 100, 0.1    
+    actions = list(range(-10*lot, 11*lot, lot))
+    qlearner = QMatrix(actions)    
+    exchange = StockExchange(stock, lot, tick, impact=0)       
     
+    reward = 0
+    state = (stock.get_price(), 0)
+    
+    iter_count = 0
+    cumulative_wealth = 0    
+    
+    # for initial training and burn in
+    for i in range(1000000):
+        order = qlearner.learn(reward, state)
+        
+        # when the exchange execute, it makes an impact on stock price
+        transaction_cost = exchange.execute(order)        
+        new_price, pnl = exchange.simulate_stock_price()
+        
+        delta_wealth = pnl + transaction_cost
+        iter_count += 1
+        cumulative_wealth += delta_wealth        
+        
+        reward = delta_wealth - util_const / 2 * (delta_wealth - cumulative_wealth / iter_count)**2
+        state = (new_price, exchange.num_shares_owned)        
+    
+    # for graphing pnl after training, run again the above 10k times
     
 if __name__ == '__main__':
     main()
