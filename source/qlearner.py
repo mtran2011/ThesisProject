37bfb4f27d040a187b8d04e3f386ea27ba818a57
Minh Ha Tran <tranhaminh@gmail.com>
2017-10-07 22:04:13 -0400

    heuristic q

--- source/qlearner.py
+++ source/qlearner.py
@@ -208,3 +208,37 @@ class DQNLearner(QLearner):
             self._last_action = action          
             self._last_state = new_state
             return action
+
+class QMatrixHeuristic(QMatrix):
+    '''
+    Attributes:        
+        _dist_func (function): to calculate distance between two points (s1, a1) and (s2, a2)
+        sample_size (int): how many samples to take when heuristically averaging and estimating 
+    '''
+
+    def __init__(self, actions, dist_func, epsilon=0.1, learning_rate=0.1, discount_factor=0.999):
+        super().__init__(actions, epsilon, learning_rate, discount_factor)        
+        self._dist_func = dist_func
+        self.sample_size = 100
+    
+    def _get_q(self, state, action):
+        if (state, action) in self._Q:
+            return self._Q[(state, action)]
+        else:
+            # guess the value of Q(state, action) via sampling and averaging
+            sample_size = min(self.sample_size, len(self._Q))
+            batch = random.sample(self._Q.keys(), sample_size)
+            distances, q_vals = [], []
+            x1 = [*state, action] # coordinate of this point
+            for that_state, that_action in batch:
+                x2 = [*that_state, that_action] # coordinate of that point
+                distance = self._dist_func(x1, x2)
+                q_val = self._Q[(that_state, that_action)]
+                distances.append(distance)
+                q_vals.append(q_val)
+            
+            distances = np.array(distances)
+            q_vals = np.array(q_vals)
+            estimate = distances.dot(q_vals) / distances.sum()
+            self._Q[(state, action)] = estimate
+            return estimate
