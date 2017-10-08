ecb511506344509573b7a3affbeff4b1f1e65005
Minh Ha Tran <tranhaminh@gmail.com>
2017-10-07 22:28:13 -0400

    save

--- source/main.py
+++ source/main.py
@@ -4,15 +4,18 @@ from exchange import *
 from qlearner import *
 from environment import *
 import model_builder
+import distance_func
 
-def graph_performance(wealths, figname):    
+def graph_performance(wealths_list, agent_names):
     plt.figure()
-    plt.plot(range(len(wealths)), wealths)
-    plt.title('Agent performance in {0} steps after training'.format(len(wealths)))
+    for wealths, agent in zip(wealths_list, agent_names):
+        plt.plot(range(len(wealths)), wealths, label=agent)
+        
+    plt.title('Agent performance in {0} steps after training'.format(len(wealths_list[0])))
+    plt.legend(loc='best')
     plt.xlabel('iterations')
     plt.ylabel('cumulative wealth')
-    plt.savefig(figname)
-    plt.show()
+    plt.savefig('../figs/newfig.png')    
 
 def run_qmatrix_stock_trading():
     stock = OUStock(price=110, kappa=0.1, mu=150, sigma=0.2, tick=0.1, band=1000)
@@ -26,7 +29,28 @@ def run_qmatrix_stock_trading():
     environment.run(1e-3, int(1e4))
     # for graphing pnl after training, run again the above 5k times
     wealths = environment.run(1e-3, 5000, report=True)
-    graph_performance(wealths, 'qmatrix_performance')
+    graph_performance([wealths], ['qmatrix'])
+
+def run_heuristic_qmatrix_stock_trading():
+    stock = OUStock(price=110, kappa=0.1, mu=150, sigma=0.2, tick=0.1, band=1000)
+    lot = 100
+    actions = list(range(-3*lot, 4*lot, lot))
+    exchange = StockExchange(stock, lot=lot, impact=0, max_holding=1000)
+
+    # for simple QMatrix    
+    learner = QMatrix(actions, epsilon=0.1, learning_rate=0.5, discount_factor=0.999)
+    environment = StockTradingEnvironment(stock, learner, exchange)
+    environment.run(1e-3, int(1e4))    
+    wealths_qmatrix = environment.run(1e-3, 5000, report=True)
+    
+    # for QMatrixHeuristic
+    dist_func = lambda x1, x2: distance_func.p_norm(x1, x2, p=2)
+    learner = QMatrixHeuristic(actions, dist_func, epsilon=0.1, learning_rate=0.5, discount_factor=0.999)
+    environment = StockTradingEnvironment(stock, learner, exchange)
+    environment.run(1e-3, int(1e4))    
+    wealths_qheuristic = environment.run(1e-3, 5000, report=True)
+        
+    graph_performance([wealths_qmatrix, wealths_qheuristic], ['simple Q matrix', 'heuristic Q matrix'])
 
 def run_dqn_stock_trading():
     stock = OUStock(price=110, kappa=0.1, mu=150, sigma=0.2, tick=0.1, band=1000)
@@ -41,8 +65,7 @@ def run_dqn_stock_trading():
     environment.run(1e-3, int(1e4))
     # for graphing pnl after training, run again the above 5k times
     wealths = environment.run(1e-3, 5000, report=True)
-    graph_performance(wealths, 'simple_dqn_ff_performance')
+    graph_performance([wealths], ['simple_dqn_ff'])
     
 if __name__ == '__main__':
-    run_qmatrix_stock_trading()
-    run_dqn_stock_trading()    
+    run_heuristic_qmatrix_stock_trading()
