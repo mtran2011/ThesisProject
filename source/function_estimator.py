76af35e168291e57a876d8a407982888f4871be6
Minh Ha Tran <tranhaminh@gmail.com>
2017-10-30 14:21:33 -0400

    cubic

--- source/function_estimator.py
+++ source/function_estimator.py
@@ -45,6 +45,7 @@ class QFunctionEstimator(object):
             action (float): a scalar value for the action
         Returns:
             ndarray: the gradient with respect to self._params, evaluated at (s,a)
+                     must have the same shape as self._params
         '''
         pass
 
@@ -54,26 +55,51 @@ class CubicEstimator(QFunctionEstimator):
         _params (ndarray): the cubic coefficients serve as parameters
     '''
     def __init__(self, num_state_features):
-        '''
+        ''' Initialize self._params to a 4 x (num_state_features + 3) matrix
+        The first num_state_features columns: 4 cubic coefs for each state feature
+        Next column: 4 cubic coefs for the product of all state features
+        Next column: 4 cubic coefs for scalar-valued action a
+        Next column: 4 cubic coefs for the product of all state features x a
         Args:
             num_state_features (int): number of features in a state
         '''
-        # 4 coefs for each state feature
-        # 4 coefs for the cubic applied on product of state features
-        # 4 coefs for the cubic on the scalar action
-        # 4 coefs for the cubic on the product of a and all state features
-        self._num_state_features = num_state_features
-        self._params = np.zeros(4 * num_state_features + 12)
+        self._params = np.zeros((4, num_state_features + 3))
 
     # Override base class abstractmethod
-    def estimate_q(self, state, action):        
-        m, q = len(state), 0
-        if m != self._num_state_features:
-            raise ValueError('the length if state input is wrong')
-        for i in range(m):
-            q += np.polyval(self._params[4*i:4*i+4], state[i])
+    def get_params(self):
+        return self._params
+
+    # Override base class abstractmethod
+    def set_params(self, params):
+        if params.shape != self._params.shape:
+            raise ValueError('params ndarray shape is wrong')
+        self._params = params
+    
+    # Override base class abstractmethod
+    def estimate_q(self, state, action):
+        n, q = len(state), 0
+        if n != (self._params.shape[1]-3):
+            raise ValueError('the length of state input is wrong')
+        for i in range(n):
+            q += np.polyval(self._params[:,i], state[i])
         x = np.prod(state)
-        q += np.polyval(self._params[4*m:4*m+4], x)
-        q += np.polyval(self._params[4*m+4:4*m+8], action)
-        q += np.polyval(self._params[4*m+8:4*m+12], action*x)
+        q += np.polyval(self._params[:,n], x)
+        q += np.polyval(self._params[:,n+1], action)
+        q += np.polyval(self._params[:,n+2], action*x)
         return q
+    
+    # Override base class abstractmethod
+    def eval_gradient(self, state, action):
+        n = len(state)
+        if n != (self._params.shape[1]-3):
+            raise ValueError('the length of state input is wrong')
+        grad = np.zeros(self._params.shape)
+        for row in range(4):
+            for col in range(n):
+                grad[row,col] = state[col]**(3-row)
+        x = np.prod(state)
+        for row in range(4):
+            grad[row,n] = x**(3-row)
+            grad[row,n+1] = action**(3-row)
+            grad[row,n+2] = (action*x)**(3-row)
+        return grad
