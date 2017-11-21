fa39dd6cd55adfeeebc1ff3a957975e36e42339a
Student <tranhaminh@gmail.com>
2017-11-21 00:08:08 -0500

    update

--- source/kernel_functions.py
+++ source/kernel_functions.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-def p_norm(x1, x2, p=2):
+def inverse_norm_p(x1, x2, p=2):
     '''
     Args:
         x1 (list): list of coordinates of the first point
         x2 (list): list of coordinates of the second point
     Returns:
-        float: distance in L1 norm between x1 and x2
+        float: inverse of distance in L-p norm between x1 and x2
     '''
     x1 = np.array(x1)
     x2 = np.array(x2)
-    return np.linalg.norm(x1 - x2, ord=p)
+    return 1 / np.linalg.norm(x1 - x2, ord=p)
--- source/main.py
+++ source/main.py
@@ -2,20 +2,19 @@ import itertools
 import matplotlib.pyplot as plt
 from stock import OUStock, GBMStock
 from exchange import StockExchange, StockOptionExchange
-from qlearner import QMatrix, QMatrixHeuristic, SemiGradQLearner, DQNLearner
+from qlearner import TabularQMatrix, KernelSmoothingQMatrix
 from environment import StockTradingEnvironment, OptionHedgingEnvironment
 from function_estimator import PairwiseLinearEstimator
 from option import EuropeanStockOption
-import distance_func
-# import model_builder
+import kernel_functions
 
 def graph_performance(wealths_list, agent_names, ntrain):
     linestyles = itertools.cycle(['-', '--', '-.', ':'])
     colors = itertools.cycle(['b', 'g', 'r', 'c', 'm', 'y', 'k'])
     
     plt.figure()
-    for wealths, agent in zip(wealths_list, agent_names):
-        plt.plot(range(len(wealths)), wealths, label=agent, 
+    for wealths, agent_name in zip(wealths_list, agent_names):
+        plt.plot(range(len(wealths)), wealths, label=agent_name, 
                  linestyle=next(linestyles),
                  color=next(colors))
     
@@ -23,7 +22,7 @@ def graph_performance(wealths_list, agent_names, ntrain):
     plt.title('Performance with ntrain = {0:,} and ntest = {1:,}'.format(ntrain, ntest))
     plt.legend(loc='best')
     plt.xlabel('iterations of testing runs')
-    plt.ylabel('cumulative wealth')
+    plt.ylabel('performance measure')
     plt.savefig('../figs/newfig.png')
 
 def make_stock_exchange():
@@ -32,7 +31,7 @@ def make_stock_exchange():
     actions = list(range(-3*lot, 4*lot, lot))
     exchange = StockExchange(stock, lot=lot, impact=0, max_holding=100)
     return actions, exchange
-
+'''
 def make_option_exchange():
     stock = GBMStock(price=10, mu=0.005, sigma=0.01, tick=0.1, band=1000)
     option = EuropeanStockOption(stock, k=10, tau=(1e6+5010), r=0.001, is_call=True)
@@ -46,41 +45,44 @@ def run_qmatrix_option_hedging():
     util, ntrain, ntest = 1e-3, int(1e6), 5000
 
     # for QMatrixHeuristic
-    dist_func = lambda x1, x2: distance_func.p_norm(x1, x2, p=2)
-    qheuristic_learner = QMatrixHeuristic(actions, dist_func, epsilon=0.1, learning_rate=0.5, discount_factor=0.999)
-    environment = OptionHedgingEnvironment(qheuristic_learner, exchange)
+    kernel = lambda x1, x2: kernel_functions.inverse_norm_p(x1, x2, p=2)
+    smoothing_qlearner = QMatrixHeuristic(actions, kernel, epsilon=0.1, learning_rate=0.5, discount_factor=0.999)
+    environment = OptionHedgingEnvironment(smoothing_qlearner, exchange)
     environment.run(util, ntrain)
     wealths_qheuristic, deltas, share_holdings = environment.run(util, ntest, report=True)
 
     graph_performance([deltas, share_holdings], ['option delta', 'scaled share holding'], ntrain)
-
+'''
 def run_qmatrix_stock_trading():
     actions, exchange = make_stock_exchange()
-    util, ntrain, ntest = 1e-3, int(1e4), 5000
+    util, ntrain, ntest = 1e-3, int(1e5), 5000
     
-    # for SemiGradQLearner    
+    # for SemiGradQLearner
+    '''
     qfunc_estimator = PairwiseLinearEstimator(num_state_features=2)
     qgrad_learner = SemiGradQLearner(actions, qfunc_estimator, epsilon=0.1, learning_rate=1e-5, discount_factor=0.999)
     environment = StockTradingEnvironment(qgrad_learner, exchange)
     environment.run(util, ntrain)    
     wealths_semigrad = environment.run(util, ntest, report=True)
     print(qfunc_estimator.get_params())
+    '''
 
-    # for simple QMatrix
-    qmatrix_learner = QMatrix(actions, epsilon=0.1, learning_rate=0.5, discount_factor=0.999)
-    environment = StockTradingEnvironment(qmatrix_learner, exchange)
+    epsilon, learning_rate, discount_factor = 0.1, 0.5, 0.999
+    # for simple TabularQMatrix
+    tabular_qlearner = TabularQMatrix(actions, epsilon, learning_rate, discount_factor)
+    environment = StockTradingEnvironment(tabular_qlearner, exchange)
     environment.run(util, ntrain)
-    wealths_qmatrix = environment.run(util, ntest, report=True)
+    wealths_tabular_qmatrix = environment.run(util, ntest, report=True)
 
-    # for QMatrixHeuristic
-    dist_func = lambda x1, x2: distance_func.p_norm(x1, x2, p=2)
-    qheuristic_learner = QMatrixHeuristic(actions, dist_func, epsilon=0.1, learning_rate=0.5, discount_factor=0.999)
-    environment = StockTradingEnvironment(qheuristic_learner, exchange)
+    # for KernelSmoothingQMatrix using inverse L2 distance
+    kernel = lambda x1, x2: kernel_functions.inverse_norm_p(x1, x2, p=2)
+    smoothing_qlearner = KernelSmoothingQMatrix(actions, kernel, epsilon, learning_rate, discount_factor)
+    environment = StockTradingEnvironment(smoothing_qlearner, exchange)
     environment.run(util, ntrain)
-    wealths_qheuristic = environment.run(util, ntest, report=True)
+    wealths_smoothing_qmatrix = environment.run(util, ntest, report=True)
 
-    graph_performance([wealths_qmatrix, wealths_qheuristic, wealths_semigrad],
-                      ['discrete Q matrix', 'heuristic Q matrix', 'semigrad Q learner'], ntrain)    
+    graph_performance([wealths_tabular_qmatrix, wealths_smoothing_qmatrix],
+                      ['tabular Q matrix', 'smoothing Q matrix'], ntrain)
 
 def run_dqn_stock_trading():
     actions, exchange = make_stock_exchange()
@@ -95,4 +97,4 @@ def run_dqn_stock_trading():
     graph_performance([wealths], ['simple_dqn_feed_forward'], ntrain)    
 
 if __name__ == '__main__':
-    run_qmatrix_option_hedging()
+    run_qmatrix_stock_trading()
